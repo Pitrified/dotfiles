7f6748e15175407b457fb7325736f6670ea88cf2
Pitrified <nobilipietro@gmail.com>
2019-04-30 12:05:53 +0200

    Add alias le for "exa -lh --git"

--- bash/bash_aliases.bash
+++ bash/bash_aliases.bash
@@ -4,6 +4,8 @@ mkcdir ()
     cd -P -- "$1"
 }
 
+alias le="exa -lh --git"
+
 # Stash your environment variables in ~/.localrc. This means they'll stay out
 # of your main dotfiles repository (which may be public, like this one), but
 # you'll have access to them in your scripts.
