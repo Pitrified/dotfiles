e63aa021bdb64e1c8c6d8ea6a5954837291c44f5
Pitrified <nobilipietro@gmail.com>
2022-06-11 18:26:01 +0200

    Add gc alias for git commit

--- git/aliases.bash
+++ git/aliases.bash
@@ -1,6 +1,8 @@
 gitid () {
     if [[ $# -eq 2 ]]; then
         # magic to check for a substring in a string
+        # this is neat because usually there are no @ in the username
+        # so if you put them swapped this will notice
         if [[ "$1" == *"@"* ]]; then
             git config user.email "$1"
             git config user.name "$2"
@@ -15,5 +17,6 @@ gitid () {
 }
 
 
+alias gc="git commit"
 alias gca="git commit -a"
 alias gs="git status"
