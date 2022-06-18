c45500ded6f4ba9bde72b06f20bc32c26fbd0bbc
Pitrified <nobilipietro@gmail.com>
2022-06-18 19:09:14 +0200

    Add more exa aliases

--- bash/aliases_exa.bash
+++ bash/aliases_exa.bash
@@ -1,26 +1,54 @@
+# super fast useful info with exa
+alias le="exa --long --git --all"
+
+# use exa --tree
 lt ()
 {
     if [[ $# -eq 0 ]]
     then
-        exa --long --git --all --tree --level=2
+        exa --long --git --all --tree --level=2 --ignore-glob='.git|__pycache__|.mypy_cache'
     fi
 
+    # here we do not ignore glob: `lt 3` can be used to show all the tree
     if [[ $# -eq 1 ]]
     then
         exa --long --git --all --tree --level="$1"
     fi
 
+    # if you do want to ignore glob with defaults, call `lt 3 ""` to fall in this branch
     if [[ $# -eq 2 ]]
     then
-        exa --long --git --all --tree --level="$1" --ignore-glob=.git"|$2"
+        exa --long --git --all --tree --level="$1" --ignore-glob='.git|__pycache__|.mypy_cache'"|$2"
     fi
 
     if [[ $# -eq 3 ]]
     then
-        exa --long --git --all --tree --level="$1" --ignore-glob=.git"|$2" "$3"
+        exa --long --git --all --tree --level="$1" --ignore-glob='.git|__pycache__|.mypy_cache'"|$2" "$3"
     fi
 }
 
-# alias le="exa -lh --git"
-alias le="exa --long --git --all"
-# alias lt="exa --long --git --all --tree --level=2"
+# use exa --tree without git info
+ltn ()
+{
+    if [[ $# -eq 0 ]]
+    then
+        exa --long --all --tree --level=2 --ignore-glob='.git|__pycache__|.mypy_cache'
+    fi
+
+    # here we do not ignore glob: `lt 3` can be used to show all the tree
+    if [[ $# -eq 1 ]]
+    then
+        exa --long --all --tree --level="$1"
+    fi
+
+    # if you do want to ignore glob with defaults, call `lt 3 ""` to fall in this branch
+    if [[ $# -eq 2 ]]
+    then
+        exa --long --all --tree --level="$1" --ignore-glob='.git|__pycache__|.mypy_cache'"|$2"
+    fi
+
+    if [[ $# -eq 3 ]]
+    then
+        exa --long --all --tree --level="$1" --ignore-glob='.git|__pycache__|.mypy_cache'"|$2" "$3"
+    fi
+}
